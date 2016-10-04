b0bd9f815ee484cfabab9c27b9049b129c34d77a
Andoni Carmona <acarmona@omitsis.com>
2016-10-04 20:11:23 +0200

    initial commit

---
+++ app/src/main/java/com/appnd/masterdetail/BookListActivity.java
@@ -0,0 +1,28 @@
+package com.appnd.masterdetail;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
+
+public class BookListActivity extends AppCompatActivity {
+
+    private ListView mList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+
+        //Intiate toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+}
